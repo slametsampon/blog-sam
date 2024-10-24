7d4012518ed72be77f2c63c4b6d18500dd20a332
Slamet <slamet@pon.co.id>
2024-10-24 12:40:46 +0700

    Remove Activities

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/activities', title: 'Activities' },
+  //{ href: '/activities', title: 'Activities' },
   { href: '/blog', title: 'Blog' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
